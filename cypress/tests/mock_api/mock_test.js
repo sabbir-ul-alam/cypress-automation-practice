31f0802171715f994384b5440aa1246fb22586aa
unknown <sabbir.ul.alam.sabbir@gmail.com>
2025-05-06 16:30:59 +0600

    Add a second mock test to change req url

--- cypress/tests/mock_api/mock_test.js
+++ cypress/tests/mock_api/mock_test.js
@@ -1,6 +1,6 @@
 describe('Mock api call', function () {
 
-    it('First mock test', function () {
+    it('First mock test to mock responsee', function () {
 
 
         cy.intercept({
@@ -16,14 +16,40 @@ describe('Mock api call', function () {
                     "aisle": "2301"
                 }]
 
-            }).as('bookretrievals')
+            }).as('bookresponse')
         cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
         cy.get("button[class='btn btn-primary']").click()
-        cy.wait('@bookretrievals').then(({ request, response }) => {
+        cy.wait('@bookresponse').then(({ request, response }) => {
             cy.get('tr').should('have.length', response.body.length + 1)
 
         })
         cy.get('p').should('have.text', 'Oops only 1 Book available')
 
-    })
+    });
+
+
+    it("Mock request", function () {
+        cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
+
+        cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+            (req) => {
+                req.url = "https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=dfgdfgdfgdfg"
+
+                req.continue((res)=>{
+                    expect(res).to.not.equal(200)
+            })
+    }).as("unauthorizedUrl")
+
+        cy.get("button[class='btn btn-primary']").click()
+        // cy.wait('@unauthorizedUrl').its('response.statusCode')
+        // .should('eq',200);
+        cy.wait('@unauthorizedUrl').then((interception) => {
+            // ✅ Assert that the URL was changed
+            expect(interception.request.url).to.include('AuthorName=shetty');
+          
+            // ✅ Print it
+            // console.log(interception.response.body);
+          });
+
+    });
 })
